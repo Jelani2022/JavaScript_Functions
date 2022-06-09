8c7f55ad2c7ce16714b214c0f2238d14db807e0c
Jelani2022 <jtwash2@gmail.com>
2022-06-09 16:06:43 -0400

    ex2

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+//let n = prompt("Where would we like to count to?")
 
+//function printOdds(count){
+  //  for (let i = 1; i<= count; i++){
+    //    if
+    //}
+
+
+//}
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+function checkAge(age){
+    let aboveSixteen = "You can drive!"
+    let belowSixteen = "Sorry, but you need to wait until you're 16."
+    if (age >= 16){
+        console.log(aboveSixteen)
+    } else {
+        console.log(belowSixteen)
+    }
+}
+checkAge(20)
+
+// Exercise 3
+
+
+// Exercise 4
+
+
+// Exercise 5
